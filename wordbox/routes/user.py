9e4f89c97d6feebc400a29fd005c0adb2e7a31a6
Gary Briceño <gary.briceno@gmail.com>
2021-04-28 21:47:49 -0500

    Update user.py

--- wordbox/routes/user.py
+++ wordbox/routes/user.py
@@ -6,19 +6,24 @@ from wordbox.model.user import UserModel
 class User(Resource):
 
     def get(self, id):
-        user = UserModel.find_by_id(id)
-        if user:
-            return user.json()
-        return {'message': 'User not found'}, 404
+        try:
+            user = UserModel.find_by_id(id)
+            if user:
+                return user.json()
+            return {'message': 'User not found'}, 404
+        except Exception:
+            return {'message': 'Server problem'}, 500
 
     def post(self):
-        req = request.get_json()
-        telefonos = req.get("telefonos", "")
-        telefonos = ",".join(telefonos)
-        user = UserModel(nombre=req.get("nombre",""),
-                         apellido=req.get("apellido",""),
-                         direccion=req.get("direccion",""),
-                         telefonos=telefonos
-                         )
-        id = user.save()
-        return {"id": str(id)}
+        try:
+            req = request.get_json()
+            telefonos = ",".join(req.get("telefonos", ""))
+            user = UserModel(nombre=req.get("nombre",""),
+                             apellido=req.get("apellido",""),
+                             direccion=req.get("direccion",""),
+                             telefonos=telefonos
+                             )
+            id = user.save()
+            return {"id": str(id)}
+        except Exception:
+            return {'message': 'Server problem'}, 500
